cdbe66e518f5eb092399ec1e87df122c884815a3
Ruben Sandström <rubensandstrom@gmail.com>
2022-12-02 10:44:07 +0100

    day2

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/rubensandstrom/Dokument/Programmering/AOC/advent_of_code_2022/day1/target/debug/day1: /home/rubensandstrom/Dokument/Programmering/AOC/advent_of_code_2022/day1/src/main.rs
--- day1/target/debug/deps/day1-bb42ab0b3cbb70af.d
+++
@@ -1,5 +0,0 @@
-/home/rubensandstrom/Dokument/Programmering/AOC/advent_of_code_2022/day1/target/debug/deps/day1-bb42ab0b3cbb70af: src/main.rs
-
-/home/rubensandstrom/Dokument/Programmering/AOC/advent_of_code_2022/day1/target/debug/deps/day1-bb42ab0b3cbb70af.d: src/main.rs
-
-src/main.rs:
