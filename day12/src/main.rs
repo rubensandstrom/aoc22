914eb0c849ec6a4c32ad8fb741d00b162d50bd21
Ruben <rubensandstrom@gmail.com>
2022-12-18 01:26:43 +0100

    day12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -4,9 +4,6 @@ type Coord = (usize, usize);
 
 fn parse ( input: &str ) -> (Vec<Vec<i32>>, Coord, Coord) {
 
-    let input = fs::read_to_string(input).expect("Couldn't read file!");
-
-    
     let mut start: Coord = (0, 0);
     let mut goal: Coord = (0, 0);
     
@@ -15,8 +12,16 @@ fn parse ( input: &str ) -> (Vec<Vec<i32>>, Coord, Coord) {
     for (row, l) in input.lines().enumerate() {
         let mut v = vec![];
         for (col, c) in l.chars().enumerate() {
-            if c == 'S' { start = (row, col); }
-            if c == 'E' { goal = (row, col); }
+            if c == 'S' { 
+                start = (row, col); 
+                v.push(0);
+                continue;
+            }
+            if c == 'E' { 
+                goal = (row, col); 
+                v.push('z' as i32 - 'a' as i32);
+                continue;
+            }
             v.push(c as i32 - 'a' as i32);
         }
         parsed_input.push(v);
@@ -24,7 +29,7 @@ fn parse ( input: &str ) -> (Vec<Vec<i32>>, Coord, Coord) {
     return (parsed_input, start, goal);
 }
 
-fn task1 ( input: Vec<Vec<i32>>, start: Coord, goal: Coord ) -> i32 {
+fn bfs ( input: Vec<Vec<i32>>, start: Coord, goal: Coord ) -> Vec<Coord> {
 
     let mut visited: HashMap<Coord, Vec<Coord>> = HashMap::new();
     let mut to_visit = vec![]; // might change to que if grows to big
@@ -35,12 +40,11 @@ fn task1 ( input: Vec<Vec<i32>>, start: Coord, goal: Coord ) -> i32 {
     while !to_visit.is_empty() {
         let (row, col) = to_visit.remove(0); 
         let path = visited[&(row, col)].clone();
+        if (row, col) == goal { return path.clone(); }
 
-        println!("{:?}", to_visit);
         if row > 0 {
             if !visited.contains_key(&(row-1, col)) {
-                if (row-1, col) == goal { return [path.clone(), vec![(row-1, col)]].concat().len() as i32; }
-                else if (row, col) == start || (-1..=1).contains(&(input[row][col] - input[row-1][col])) {
+                if input[row-1][col] - input[row][col] <= 1 {
 
                     visited.insert((row-1, col), [path.clone(), vec![(row-1, col)]].concat());
                     to_visit.push((row-1, col));
@@ -49,8 +53,7 @@ fn task1 ( input: Vec<Vec<i32>>, start: Coord, goal: Coord ) -> i32 {
         }
         if col > 0 {
             if !visited.contains_key(&(row, col-1)) {
-                if (row, col-1) == goal { return [path.clone(), vec![(row, col-1)]].concat().len() as i32; }
-                else if (row, col) == start || (-1..=1).contains(&(input[row][col] - input[row][col-1])) {
+                if input[row][col-1] - input[row][col] <= 1 {
 
                     visited.insert((row, col-1), [path.clone(), vec![(row, col-1)]].concat());
                     to_visit.push((row, col-1));
@@ -59,8 +62,7 @@ fn task1 ( input: Vec<Vec<i32>>, start: Coord, goal: Coord ) -> i32 {
         }
         if row < input.len()-1 {
             if !visited.contains_key(&(row+1, col)) {
-                if (row+1, col) == goal { return [path.clone(), vec![(row+1, col)]].concat().len() as i32; }
-                else if (row, col) == start || (-1..=1).contains(&(input[row][col] - input[row+1][col])) {
+                if input[row+1][col] - input[row][col] <= 1 {
 
                     visited.insert((row+1, col), [path.clone(), vec![(row+1, col)]].concat());
                     to_visit.push((row+1, col));
@@ -69,8 +71,7 @@ fn task1 ( input: Vec<Vec<i32>>, start: Coord, goal: Coord ) -> i32 {
         }
         if col < input[0].len()-1 {
             if !visited.contains_key(&(row, col+1)) {
-                if (row, col+1) == goal { return [path.clone(), vec![(row, col+1)]].concat().len() as i32; }
-                else if (row, col) == start || (-1..=1).contains(&(input[row][col] - input[row][col+1])) {
+                if input[row][col+1] - input[row][col] <= 1 {
 
                     visited.insert((row, col+1), [path.clone(), vec![(row, col+1)]].concat());
                     to_visit.push((row, col+1));
@@ -79,41 +80,59 @@ fn task1 ( input: Vec<Vec<i32>>, start: Coord, goal: Coord ) -> i32 {
         }
 
     }
-    println!("Couldn't find path!");
-    return 0;
+    return vec![];
 }
 
+fn task1(input: &str) -> i32 {
+
+    let (input, start, goal) = parse(input);
+    let path = bfs(input.clone(), start, goal);
+
+    return path.len() as i32;
+}
 fn task2(input: &str) -> i32 {
 
-    return 0;
+    let (input, _start, goal) = parse(input);
+    let mut lo = vec![];
+    
+    for row in 0..input.len() {
+        for col in 0..input[0].len() { 
+            if input[row][col] == 0 {
+                lo.push((row, col));
+            }
+        }
+    }
+
+    let mut routes = vec![];
+    for a in lo {
+        let route = bfs(input.clone(), a, goal).len();
+        if route != 0 { routes.push(route); }
+    }
+
+    routes.sort();
+    return routes[0] as i32;
 }
 
 fn main() {
 
-    let (input, start, goal) = parse("input");
-    println!( "start: {:?}", start );
-    println!( "goal: {:?}", goal );
-
-    println!("{}", task1(input, start, goal));
-
+    let input = fs::read_to_string("input").expect("Couldn't read file!");
+    println!("{}", task1(&input));
+    println!("{}", task2(&input));
 }
 
 #[cfg(test)]
 mod test {
-    use super::{parse, task1, task2};
+    use super::{ task1, task2 };
     use std::fs;
 
     #[test]
     fn test1() {
-        let ( test, start, goal ) = parse("test"); 
-        println!("{:?}", test);
-        println!("{:?}", start);
-        println!("{:?}", goal);
-        assert_eq!(task1(test, start, goal), 31);
+        let test = fs::read_to_string("test").expect("Couldn't read file!");
+        assert_eq!(task1(&test), 31);
     }
     #[test]
     fn test2() {
         let test = fs::read_to_string("test").expect("Couldn't read file!");
-        assert_eq!(task2(&test), 0);
+        assert_eq!(task2(&test), 29);
     }
 }
